dd695b63e3e0f70bd180667077ee2a8822116411
Kamil Prorok <kamilxprorok@gmail.com>
2025-01-26 22:49:41 +0100

    Created a Database class that creates tables in a SQLITE database

---
+++ src/database/Database.cpp
@@ -0,0 +1,41 @@
+﻿//
+// Created by Kamil Prorok on 26.01.2025.
+//
+
+#include "Database.h"
+
+#include <cstdio>
+
+namespace db {
+  Database::Database() {
+    if (sqlite3_open(DB_NAME, &db) != SQLITE_OK) {
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      return;
+    }
+
+    this->create_tables();
+  }
+
+  Database::~Database() {
+    if (sqlite3_close(db) != SQLITE_OK) {
+      fprintf(stderr, "Can't close database: %s\n", sqlite3_errmsg(db));
+    }
+  }
+
+  void Database::execute_sql(const char *sql) {
+    char *err_msg = nullptr;
+    if (sqlite3_exec(db, sql, nullptr, nullptr, &err_msg) != SQLITE_OK) {
+      fprintf(stderr, "SQL error: %s\n", err_msg);
+      sqlite3_free(err_msg);
+    }
+  }
+
+
+  void Database::create_tables() {
+    this->execute_sql(CREATE_DECKS_TABLE_SQL);
+    this->execute_sql(CREATE_CARDS_TABLE_SQL);
+    this->execute_sql(CREATE_ANSWERS_TABLE_SQL);
+    this->execute_sql(CREATE_PROGRESS_TABLE_SQL);
+  }
+
+} // db
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+﻿#include <cstdio>
+#include <sqlite3.h>
+
+#include "database/Database.h"
+
+int main() {
+  db::Database db;
+  return 0;
+}
